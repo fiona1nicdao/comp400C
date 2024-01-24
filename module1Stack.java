4bd4ea35310ea8967bd97f431799122e1054c2f1
FionaN <fiona1941nicdao@gmail.com>
2024-01-24 11:05:16 -0600

    add class exercises for module 1

---
+++ module1Stack.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+/*
+ * A) Write a method that takes in a Stack and prints the elements in reverse order,
+ *     while keeping the original stack unchanged (at the end of the process)
+ *      Example input: [1,2,3,4]
+ *      Example output: 4,3,2,1
+ * B) Write a method that inserts an element into the stack at a specific index (assume
+ *     that the element with index 0 is at the bottom of the stack)
+ *      Example input: [1,2,3,5[, index=3, value=4 
+ *       Example output: [1,2,3,4,5[
+ * C) Write a method that returns the smallest index of an element in a stack
+ *      Example input: [1,2,3,4,1,2,3[, value=3
+ *      Example output: 2
+ * D) Write a method that uses a stack to determine if a string is a palindrome or not
+ *      Example input: "racecar"
+ *      Example output: true
+ */
+
+public class module1Stack {
+    
+    public static void main(String[] args){
+        //create Stack using built in Java class
+        
+        //push elements into stack
+        
+        //call methods
+        
+    }
+    
+
+}
